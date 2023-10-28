b65b08170d62dbf0ea724c3bfdb6b41981d287fa
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-10-28 23:21:14 +0600

    banner done, information done, navbar done

--- src/pages/Home/home/Home.jsx
+++ src/pages/Home/home/Home.jsx
@@ -1,7 +1,11 @@
+import Contact from "./Contact";
+import Information from "./Information";
 
 const Home = () => {
     return (
         <div>
+            <Information/>
+            <Contact/>
             home page
         </div>
     );
---
+++ src/pages/Home/home/Information.jsx
@@ -0,0 +1,34 @@
+import { FaRegClock } from 'react-icons/fa';
+import { SlLocationPin } from 'react-icons/Sl';
+import { SlCallOut } from 'react-icons/Sl';
+
+const Information = () => {
+    return (
+        <div className="container mx-auto flex justify-between gap-8 text-white">
+            <div className='bg-[#811A64] flex gap-5 w-[364px] h-[202px] p-12 rounded-xl'>
+                <FaRegClock className='text-5xl' />
+                <div>
+                    <h3 className='text-2xl font-bold'>Opening Hours</h3>
+                    <p className='text-md mt-4'>24/7 Active Instructors for 100+ Courses</p>
+                </div>
+            </div>
+            <div className='bg-[#F1946D] flex gap-5 w-[364px] h-[202px] p-12 rounded-xl'>
+                <SlLocationPin className='text-5xl' />
+                <div>
+                    <h3 className='text-2xl font-bold'>Our Locations</h3>
+                    <p className='text-md mt-4'>24/7 We Can Provide service To All Over The World</p>
+                </div>
+            </div>
+            <div className='bg-[#0E6159] flex gap-5 w-[364px] h-[202px] p-12 rounded-xl'>
+                <SlCallOut className='text-5xl' />
+                <div>
+                    <h3 className='text-2xl font-bold'>Contact Us</h3>
+                    <p className='text-md mt-4'>+88 01750 00 00 00
+                        edu@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Information;
