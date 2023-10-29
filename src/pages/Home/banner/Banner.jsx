b9faf78e00f26771bbc8120cc884e7015bd028cd
Emon <emon.mhk69@gmail.com>
2023-10-29 23:37:05 +0600

    navbar done

--- src/pages/Home/banner/Banner.jsx
+++ src/pages/Home/banner/Banner.jsx
@@ -1,7 +1,7 @@
 
 const Banner = () => {
     return (
-        <div className="text-white">
+        <div className="">
             <div className="mt-20 flex justify-between gap-44">
                 <div>
                     <h2 className="text-5xl text-justify py-2">Best EDUCATIONAL</h2>
@@ -15,7 +15,7 @@ const Banner = () => {
                     <img className="w-[500px] " src="https://i.ibb.co/3173Vgy/image-1.png"/>
                 </div>
             </div>
-            <button className="bg-[#F7A582] mb-20 py-4 px-8 font-bold rounded-xl">All Service</button>
+            <button className="btn btn-sm">All Service</button>
         </div>
     );
 };
