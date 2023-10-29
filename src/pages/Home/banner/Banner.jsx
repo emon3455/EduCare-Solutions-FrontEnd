288bc72ff43bdf78b71616e3585ed671cb68f96b
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-10-30 00:22:19 +0600

    banner fixed responsiveness

--- src/pages/Home/banner/Banner.jsx
+++ src/pages/Home/banner/Banner.jsx
@@ -1,21 +1,20 @@
 
 const Banner = () => {
     return (
-        <div className="">
-            <div className="mt-20 flex justify-between gap-44">
+        <div className="bg-primary">
+            <div className="px-5 md:px-48  flex flex-col-reverse md:flex-row md:gap-10 justify-center text-white py-4 md:py-24">
                 <div>
-                    <h2 className="text-5xl text-justify py-2">Best EDUCATIONAL</h2>
-                    <h2 className="text-5xl text-justify py-2">Consultation CENTER </h2>
-                    <h2 className="text-5xl text-justify py-2">FROM BANGLADESH</h2>
-                    <p className="mt-3 text-sm">
+                    <h2 className="text-2xl lg:text-5xl py-1 md:py-2">Best EDUCATIONAL Consultation CENTER FROM BANGLADESH</h2>
+                    <p className="mt-3 text-md">
                         Lorem Ipsum is simply dummy text they are printing typesetting <br /> has been the industry standard.
                     </p>
+                    <button className="text-sm font-bold px-7 py-4 rounded-xl bg-secondary text-white my-3">All Service</button>
                 </div>
-                <div>
-                    <img className="w-[500px] " src="https://i.ibb.co/3173Vgy/image-1.png"/>
+                <div className="flex justify-center items-center">
+                    <img className="w-[100%] " src="https://i.ibb.co/3173Vgy/image-1.png" />
                 </div>
             </div>
-            <button className="btn btn-sm">All Service</button>
+
         </div>
     );
 };
