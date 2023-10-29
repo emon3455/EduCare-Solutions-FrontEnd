ca2bf678b0827d8b33e7317ad39a2ec4662cdee3
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-10-30 00:27:18 +0600

    banner fixed responsiveness

--- src/pages/Home/home/Information.jsx
+++ src/pages/Home/home/Information.jsx
@@ -4,22 +4,22 @@ import { SlCallOut } from 'react-icons/Sl';
 
 const Information = () => {
     return (
-        <div className="container mx-auto flex justify-between gap-8 text-white">
-            <div className='bg-[#811A64] flex gap-5 w-[364px] h-[202px] p-12 rounded-xl'>
+        <div className="container mx-auto my-12 flex flex-col lg:flex-row justify-between gap-8 text-white">
+            <div className='bg-[#811A64] flex gap-5 w-full lg:w-[364px] h-full lg:h-[202px] p-12 rounded-xl'>
                 <FaRegClock className='text-5xl' />
                 <div>
                     <h3 className='text-2xl font-bold'>Opening Hours</h3>
                     <p className='text-md mt-4'>24/7 Active Instructors for 100+ Courses</p>
                 </div>
             </div>
-            <div className='bg-[#F1946D] flex gap-5 w-[364px] h-[202px] p-12 rounded-xl'>
+            <div className='bg-[#F1946D] flex gap-5 w-full lg:w-[364px] h-full lg:h-[202px] p-12 rounded-xl'>
                 <SlLocationPin className='text-5xl' />
                 <div>
                     <h3 className='text-2xl font-bold'>Our Locations</h3>
                     <p className='text-md mt-4'>24/7 We Can Provide service To All Over The World</p>
                 </div>
             </div>
-            <div className='bg-[#0E6159] flex gap-5 w-[364px] h-[202px] p-12 rounded-xl'>
+            <div className='bg-[#0E6159] flex gap-5 w-full lg:w-[364px] h-full lg:h-[202px] p-12 rounded-xl'>
                 <SlCallOut className='text-5xl' />
                 <div>
                     <h3 className='text-2xl font-bold'>Contact Us</h3>
