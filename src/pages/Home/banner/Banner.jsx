7fe9038701caf8723bb760729b3fe77638068673
Emon <emon.mhk69@gmail.com>
2023-10-31 02:11:06 +0600

    code pulled from elina and merged

--- src/pages/Home/banner/Banner.jsx
+++ src/pages/Home/banner/Banner.jsx
@@ -5,11 +5,11 @@ const Banner = () => {
             <div className="container mx-auto">
                 <div className="flex flex-col-reverse lg:flex-row lg:justify-between items-center lg:gap-x-20">
                     <div className="w-full lg:w-1/2">
-                        <h2 className="text-2xl md:text-4xl lg:text-6xl text-justify font-semibold ">Edu-Care Solution, Your Best Learning Partner. Our Service your solution</h2>
+                        <h2 className="text-2xl md:text-4xl lg:text-5xl text-justify font-semibold ">Edu-Care Solution, Your Best Learning Partner. Our Service your solution</h2>
                         <p className="my-3 text-md">
                             Innovative Educational Solutions Tailored for Success. Transformative Learning Experiences That Inspire. Partnering for Academic Excellence and Personal Growth. Your Bridge to a Brighter Tomorrow. Unlocking Potential, One Student at a Time.
                         </p>
-                        <button className="btn-lg rounded-xl bg-secondary px-7 py-4 text-white font-bold">All Service</button>
+                        <button className="btn-primary">All Service</button>
                     </div>
                     <div className="w-full lg:w-1/2">
                         <img className="w-full rounded" src="https://i.ibb.co/3173Vgy/image-1.png" />
