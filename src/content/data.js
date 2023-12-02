8b280f0011781092cb292aebdd977ecb7ac635a9
Emon <emon.mhk69@gmail.com>
2023-12-03 00:27:54 +0600

    blogs ans sessions data added

--- src/content/data.js
+++ src/content/data.js
@@ -93,3 +93,78 @@ export const popularTeacher = [
         address: "789 Pine Street, London, UK"
     }
 ];
+
+export const blogsData = [
+    {
+        _id: "1",
+        "title": "The Future of Machine Learning",
+        "TID": 201,
+        "TName": "Elena Rodriguez",
+        "TEmail": "elena.rodriguez@example.com",
+        "blogBanner": "https://i.ibb.co/ZfC7BD6/machine-learning-banner.jpg",
+        "description": "Explore the advancements and possibilities in the field of machine learning. From deep learning to AI applications, discover how ML is shaping the future.",
+        "NoOfLike": 1200
+    },
+    {
+        _id: "2",
+        "title": "Mastering Web Development: Tips and Tricks",
+        "TID": 202,
+        "TName": "Chris Thompson",
+        "TEmail": "chris.thompson@example.com",
+        "blogBanner": "https://i.ibb.co/qFQYpqw/web-development-banner.jpg",
+        "description": "Learn the latest techniques and best practices in web development. From front-end frameworks to server-side scripting, unlock the secrets to becoming a master web developer.",
+        "NoOfLike": 850
+    },
+    {
+        _id: "3",
+        "title": "Cybersecurity in the Digital Age",
+        "TID": 203,
+        "TName": "Amit Patel",
+        "TEmail": "amit.patel@example.com",
+        "blogBanner": "https://i.ibb.co/NnQN80D/cybersecurity-banner.jpg",
+        "description": "Delve into the world of cybersecurity and its importance in the digital age. Explore the latest threats, preventive measures, and the role of ethical hacking in securing online assets.",
+        "NoOfLike": 1500
+    },
+    {
+        _id: "4",
+        "title": "Building Secure Web Applications",
+        "TID": 204,
+        "TName": "Sarah Mitchell",
+        "TEmail": "sarah.mitchell@example.com",
+        "blogBanner": "https://i.ibb.co/K2X4SbX/secure-web-apps-banner.jpg",
+        "description": "Discover the key principles and practices for building secure web applications. From encryption to secure coding, ensure your web projects are resilient against cyber threats.",
+        "NoOfLike": 1050
+    }
+]
+
+export const sessionsData = [
+    {
+        "title": "Web Development Fundamentals",
+        "TID": 301,
+        "sessionBanner": "https://i.ibb.co/2ZYP4T4/web-dev-session-banner.jpg",
+        "sessionDate": "2023-03-15",
+        "sessionPlatform": "Zoom",
+        "sessionLink": "https://zoom.us/webdev-fundamentals",
+        "sessionTime": "2 PM"
+    },
+    {
+        "title": "Machine Learning Workshop",
+        "TID": 302,
+        "sessionBanner": "https://i.ibb.co/cyYpHP3/ml-workshop-banner.jpg",
+        "sessionDate": "2023-04-10",
+        "sessionPlatform": "Microsoft Teams",
+        "sessionLink": "https://teams.com/ml-workshop",
+        "sessionTime": "10:30 AM"
+    },
+    {
+        "title": "Cybersecurity Essentials",
+        "TID": 303,
+        "sessionBanner": "https://i.ibb.co/8XJ8J6T/cybersecurity-session-banner.jpg",
+        "sessionDate": "2023-05-20",
+        "sessionPlatform": "Google Meet",
+        "sessionLink": "https://meet.google.com/cybersecurity-essentials",
+        "sessionTime": "3:45 PM"
+    }
+]
+
+
