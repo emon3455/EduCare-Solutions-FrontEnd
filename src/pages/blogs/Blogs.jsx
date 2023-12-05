20a441af7efd9719f1e4e2817dc398ef12d68455
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-12-06 00:45:51 +0600

    blogs page updated

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -7,12 +7,12 @@ const Blogs = () => {
     return (
         <div>
             <CPageBanner title={`Blogs`}/>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-7 p-2 mx-5 h-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-7 p-2 mx-5 h-auto rounded">
                 {
                     blogsData.map(blog => <div
                         key={blog._id}
                         className="border border-[#E6E6E6] rounded-lg hover:shadow-xl transition-all duration-500">
-                        <img className="w-full md:h-52" src={blog.blogBanner} alt="" />
+                        <img className="w-full md:h-52 overflow-hidden" src={blog.blogBanner} alt="" />
                         <div className="p-5">
                             <h3 className="mt-3 font-bold text-xl">{blog.title}</h3>
                             {/* <p className="text-[#6C6B6B]">Writer: {blog.TName}</p> */}
