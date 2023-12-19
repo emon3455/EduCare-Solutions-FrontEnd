4ad89cc7b502ce0a552d9c164d80ce54b4455435
Noushin-Tabassum-Elina <elinajsr05@gmail.com>
2023-12-19 23:56:26 +0600

    stats issue

--- src/components/admin/Stats.jsx
+++ src/components/admin/Stats.jsx
@@ -7,7 +7,7 @@ const Stats = ({ data }) => {
     return (
         <section className="mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-3">
-                <div className="w-20  px-8">User : {data.noOfUsers}</div>
+                <div className="">User : {data.noOfUsers}</div>
                 <div>Teacher : {data.noOfTeacher}</div>
                 <div>Students : {data.noOfStudent}</div>
                 <div>Courses : {data.noOfCourse}</div>
