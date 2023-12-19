a296a888db1d29210ab201853f31410b676c8f94
Emon <emon.mhk69@gmail.com>
2023-12-19 21:36:51 +0600

    user role edit done

---
+++ src/components/admin/users/UsersTable.jsx
@@ -0,0 +1,212 @@
+/* eslint-disable react/prop-types */
+import CButton from "../../../utils/CButton/CButton";
+import { FaUserGear } from "react-icons/fa6";
+import { FaUserGraduate } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa6";
+import { useEffect } from "react";
+import { useUpdateRoleMutation } from "../../../redux/features/user/user-api-slice";
+import Swal from "sweetalert2";
+import Loading from "../../../utils/CLoading/Loading";
+
+const UsersTable = ({ data, refetch }) => {
+
+
+    // //showing success message
+    // useEffect(() => {
+    //     if (deleteUsersIsSuccess) {
+    //         Swal.fire(
+    //             'User Deleted Successfully!',
+    //             'Success!',
+    //             'success'
+    //         )
+    //         refetch();
+    //     }
+    // }, [deleteUsersIsSuccess, refetch]);
+
+    // //showing error message
+    // useEffect(() => {
+    //     if (deleteUsersIsError) {
+    //         Swal.fire({
+    //             icon: 'error',
+    //             title: 'Oops...',
+    //             text: 'User Not Deleted, Please try again...!',
+    //         })
+    //     }
+    // }, [deleteUsersIsError]);
+
+    // const handleDelete = (id) => {
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "Once Deleted, you will not be able to revert this!",
+    //         confirmButtonText: "Delete",
+    //         showCancelButton: true,
+    //     }).then( (result) => {
+    //         if (result.isConfirmed) {
+    //             deleteAuthUser()
+    //             .then(async () => {
+    //                 try {
+    //                     await deleteUser(id)
+    //                 }
+    //                 catch (er) {
+    //                     console.log(er);
+    //                     return;
+    //                 }
+    //             }).catch((error) => {
+    //                 Swal.fire({
+    //                     icon: 'error',
+    //                     title: 'Oops...',
+    //                     text: 'User Not Deleted, Please try again...!',
+    //                 })
+    //                 console.log(error);
+    //                 return;
+    //             });
+
+    //         }
+    //     });
+
+    // }
+
+    const [
+        updateRole,
+        { isLoading: updateRoleIsLoading, isSuccess: updateRoleIsSuccess, isError: updateRoleIsError },
+    ] = useUpdateRoleMutation();
+
+    //showing success message
+    useEffect(() => {
+        if (updateRoleIsSuccess) {
+            refetch();
+            Swal.fire(
+                'Role updated Successfully!',
+                'Success!',
+                'success'
+            )
+        }
+    }, [refetch, updateRoleIsSuccess]);
+
+    //showing error message
+    useEffect(() => {
+        if (updateRoleIsError) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Role Not updated, Please try again...!',
+            })
+        }
+    }, [updateRoleIsError]);
+
+    if (updateRoleIsLoading) return <Loading />
+
+
+    const handleRoleChange = async (id, roles) => {
+        const data = {
+            id,
+            roles
+        }
+
+        try {
+            await updateRole(data)?.unwrap();
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+
+    return (
+        <section className="mx-auto">
+
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Roles</th>
+                            <th className="text-center">Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            data.map((user, indx) => <tr key={indx}>
+                                <td>{indx + 1}</td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="w-12 h-10 rounded">
+                                            <img src={user.image} alt="user Banner" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{user?.name}</td>
+                                <td>{user?.email}</td>
+                                <td>{user?.roles}</td>
+                                <td className="flex flex-col md:flex-row gap-2 justify-center items-center">
+
+                                    {/* <Link to={`/dashboard/updateUser/${user?._id}`}>
+                                        <CButton
+                                            className={'bg-orange-400 text-white rounded-full p-2'}
+                                        >
+                                            <FaPenToSquare className="text-lg" />
+                                        </CButton>
+                                    </Link> */}
+
+                                    {/* <CButton
+                                        onClick={() => {
+                                            setIndex(indx);
+                                            handleDelete(`${user?._id}`);
+                                        }}
+                                        className={'bg-red-500 text-white rounded-full p-2'}
+                                        loading={index === indx && deleteUsersIsLoading}
+                                    >
+                                        <RiDeleteBin2Fill className="text-lg" />
+                                    </CButton> */}
+
+                                    <CButton
+                                        onClick={() => handleRoleChange(user._id, "Admin")}
+                                        className={'bg-green-500 text-white rounded-full'}
+                                        style={{ padding: "1px 2px" }}
+                                        disabled={user.roles === "Admin" ? true : false}
+                                    >
+                                        <div className="flex flex-col items-center ">
+                                            <FaUserGear className="text-sm" />
+                                            <span className="text-xs">Make Admin</span>
+                                        </div>
+                                    </CButton>
+                                    <CButton
+                                        onClick={() => handleRoleChange(user._id, "Teacher")}
+                                        className={'bg-sky-500 text-white rounded-full'}
+                                        style={{ padding: "1px 2px" }}
+                                        disabled={user.roles === "Teacher" ? true : false}
+                                    >
+                                        <div className="flex flex-col items-center ">
+                                            <FaUserTie className="text-sm" />
+                                            <span className="text-xs">Make Teacher</span>
+                                        </div>
+                                    </CButton>
+                                    <CButton
+                                        onClick={() => handleRoleChange(user._id, "Student")}
+                                        className={'bg-pink-500 text-white rounded-full'}
+                                        style={{ padding: "1px 2px" }}
+                                        disabled={user.roles === "Student" ? true : false}
+                                    >
+                                        <div className="flex flex-col items-center ">
+                                            <FaUserGraduate className="text-sm" />
+                                            <span className="text-xs">Make Student</span>
+                                        </div>
+                                    </CButton>
+
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </section>
+    );
+};
+
+export default UsersTable;
--- src/pages/dashboard/admin/user/ManageUser.jsx
+++ src/pages/dashboard/admin/user/ManageUser.jsx
@@ -1,10 +1,42 @@
+import { useEffect } from "react";
+import Loading from "../../../../utils/CLoading/Loading";
+import ErrorAllert from "../../../../shared/ErrorAllert";
+import { useGetAllUserQuery } from "../../../../redux/features/user/user-api-slice";
+import CCard from "../../../../utils/CCard/CCard";
+import WarningAllert from "../../../../shared/WarningAllert";
+import UsersTable from "../../../../components/admin/users/UsersTable";
 
 
 const ManageUser = () => {
+
+
+    const { isLoading: usersIsLoading, isError: usersIsError, data: allusers, refetch } = useGetAllUserQuery();
+
+    useEffect(() => {
+        refetch()
+    }, [refetch]);
+
+    if (usersIsLoading) return <Loading />
+    if (usersIsError) return <ErrorAllert message={'Error! Something Went Wrong...!!!'} />
+
     return (
-        <div>
-            manage users
-        </div>
+        <main className="p-2 lg:p-4">
+            <CCard title={'Manage Users'}>
+                {
+                    usersIsLoading && <Loading />
+                }
+                {
+                    allusers.length == 0
+                        ?
+                        <WarningAllert message={'No Course Data Found...!!!'} />
+                        :
+                        <UsersTable
+                            data={allusers || []}
+                            refetch={refetch}
+                        />
+                }
+            </CCard>
+        </main>
     );
 };
 
