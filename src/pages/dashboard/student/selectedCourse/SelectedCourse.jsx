d9de44faa0762f9273a40770cbdd42817cf8d62f
Emon <emon.mhk69@gmail.com>
2023-12-27 00:34:00 +0600

    user issue resolved

--- src/pages/dashboard/student/selectedCourse/SelectedCourse.jsx
+++ src/pages/dashboard/student/selectedCourse/SelectedCourse.jsx
@@ -10,66 +10,68 @@ const SelectedCourse = () => {
     const dispatch = useDispatch();
 
     return (
-        <CCard title={'Selected Courses'}>
-            <div className="">
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Image</th>
-                                <th>Price</th>
-                                <th>Ratings</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                cart.length == 0 
-                                && 
-                                <WarningAllert  message={'Nothing To show! Please Add some Item First'}/>
-                            }
-                            {
-                                cart && cart.map((item) => <tr key={item._id}>
+        <div className="p-4">
+            <CCard title={'Selected Courses'}>
+                <div className="">
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Image</th>
+                                    <th>Price</th>
+                                    <th>Ratings</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    cart.length == 0
+                                    &&
+                                    <WarningAllert message={'Nothing To show! Please Add some Item First'} />
+                                }
+                                {
+                                    cart && cart.map((item) => <tr key={item._id}>
 
-                                    <td>{item?.title}</td>
-                                    <td>
-                                        <div className="avatar">
-                                            <div className="w-12 h-10 rounded">
-                                                <img src={item?.bannerURL} alt="course Banner" />
+                                        <td>{item?.title}</td>
+                                        <td>
+                                            <div className="avatar">
+                                                <div className="w-12 h-10 rounded">
+                                                    <img src={item?.bannerURL} alt="course Banner" />
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        {item?.price} $
-                                    </td>
-                                    <td>
-                                        {item?.rating}
-                                    </td>
-                                    <td>
-                                        <div className="flex flex-col lg:flex-row gap-2">
-                                            <CButton
-                                                onClick={() => dispatch(removeFromCart(item?._id))}
-                                                className={'bg-red-500 text-white rounded-full p-2'}
-                                            >
-                                                <FaTrash className="text-lg" />
-                                            </CButton>
-                                            <CButton
-                                                variant={'contained'}
-                                            >
-                                                Pay
-                                            </CButton>
-                                        </div>
-                                    </td>
-                                </tr>)
-                            }
+                                        </td>
+                                        <td>
+                                            {item?.price} $
+                                        </td>
+                                        <td>
+                                            {item?.rating}
+                                        </td>
+                                        <td>
+                                            <div className="flex flex-col lg:flex-row gap-2">
+                                                <CButton
+                                                    onClick={() => dispatch(removeFromCart(item?._id))}
+                                                    className={'bg-red-500 text-white rounded-full p-2'}
+                                                >
+                                                    <FaTrash className="text-lg" />
+                                                </CButton>
+                                                <CButton
+                                                    variant={'contained'}
+                                                >
+                                                    Pay
+                                                </CButton>
+                                            </div>
+                                        </td>
+                                    </tr>)
+                                }
 
-                        </tbody>
+                            </tbody>
 
-                    </table>
+                        </table>
+                    </div>
                 </div>
-            </div>
-        </CCard>
+            </CCard>
+        </div>
     );
 };
 
