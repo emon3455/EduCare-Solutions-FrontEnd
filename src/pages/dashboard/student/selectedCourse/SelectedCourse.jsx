d3d7b9665798abee8e8f04784a45c993ba80d0ab
Emon <emon.mhk69@gmail.com>
2023-12-26 23:41:22 +0600

    remove and display done

--- src/pages/dashboard/student/selectedCourse/SelectedCourse.jsx
+++ src/pages/dashboard/student/selectedCourse/SelectedCourse.jsx
@@ -1,9 +1,75 @@
+import { FaTrash } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart } from "../../../../redux/features/cart/cartSlice";
+import CButton from "../../../../utils/CButton/CButton";
+import CCard from "../../../../utils/CCard/CCard";
+import WarningAllert from "../../../../shared/WarningAllert";
 
 const SelectedCourse = () => {
+    const cart = useSelector(state => state.cart);
+    const dispatch = useDispatch();
+
     return (
-        <div>
-            selected course
-        </div>
+        <CCard title={'Selected Courses'}>
+            <div className="">
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Image</th>
+                                <th>Price</th>
+                                <th>Ratings</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                cart.length == 0 
+                                && 
+                                <WarningAllert  message={'Nothing To show! Please Add some Item First'}/>
+                            }
+                            {
+                                cart && cart.map((item) => <tr key={item._id}>
+
+                                    <td>{item?.title}</td>
+                                    <td>
+                                        <div className="avatar">
+                                            <div className="w-12 h-10 rounded">
+                                                <img src={item?.bannerURL} alt="course Banner" />
+                                            </div>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {item?.price} $
+                                    </td>
+                                    <td>
+                                        {item?.rating}
+                                    </td>
+                                    <td>
+                                        <div className="flex flex-col lg:flex-row gap-2">
+                                            <CButton
+                                                onClick={() => dispatch(removeFromCart(item?._id))}
+                                                className={'bg-red-500 text-white rounded-full p-2'}
+                                            >
+                                                <FaTrash className="text-lg" />
+                                            </CButton>
+                                            <CButton
+                                                variant={'contained'}
+                                            >
+                                                Pay
+                                            </CButton>
+                                        </div>
+                                    </td>
+                                </tr>)
+                            }
+
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </CCard>
     );
 };
 
